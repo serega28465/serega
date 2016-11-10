30c2941fb77055f31fe4e7ebae292f8032038cde
serega28465 <serega28465@gmail.com>
2016-11-10 14:03:58 +0200

    Update Array5.java

--- Array5.java
+++ Array5.java
@@ -10,35 +10,35 @@ public class Array5 {
 		 * це значення виявилося більше (або повідомте, що їх середні
 		 * арифметичні рівні)
 		 */
-		
+
 		double Arr1sa = 0, Arr2sa = 0;
-		
+
 		int[] Arr1 = new int[5];
-		for(int i = 0; i < Arr1.length;i++){
-			Arr1[i]=(int)(Math.random()*6);
+		for (int i = 0; i < Arr1.length; i++) {
+			Arr1[i] = (int) (Math.random() * 6);
 			System.out.print(Arr1[i] + " ");
-			
-			Arr1sa+=(double)Arr1[i]/Arr1.length;
+
+			Arr1sa += (double) Arr1[i] / Arr1.length;
 		}
 		System.out.println(" - Перший масив");
-		
+
 		int[] Arr2 = new int[5];
-		for(int i = 0; i < Arr2.length;i++){
-			Arr2[i]=(int)(Math.random()*6);
+		for (int i = 0; i < Arr2.length; i++) {
+			Arr2[i] = (int) (Math.random() * 6);
 			System.out.print(Arr2[i] + " ");
-			
-			Arr2sa+=(double)Arr2[i]/Arr2.length;
+
+			Arr2sa += (double) Arr2[i] / Arr2.length;
 		}
 		System.out.println(" - Другий масив");
-		if(Arr1sa==Arr2sa)System.out.println("Середні арифметичні двох масивів рівні");
-			else{if(Arr1sa>Arr2sa){
+		if (Arr1sa == Arr2sa)
+			System.out.println("Середні арифметичні двох масивів рівні");
+		else {
+			if (Arr1sa > Arr2sa) {
 				System.out.println("Середні арифметичні значення першого масиву більше");
-						}else{
-							System.out.println("Середні арифметичні значення другого масиву більше");
-						}
-				
-					}
-				}
+			} else {
+				System.out.println("Середні арифметичні значення другого масиву більше");
+			}
+
+		}
 	}
-
-
+}
