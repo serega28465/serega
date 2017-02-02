2bff892dd5370b52b41f87e85277c6407d33071b
serega28465 <serega28465@gmail.com>
2017-02-02 18:21:50 +0200

    Update ModelValidator.java

--- lesson18/validator/ModelValidator.java
+++ lesson18/validator/ModelValidator.java
@@ -4,7 +4,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-
+import ua.dto.form.ModelForm;
 import ua.entity.Model;
 
 import ua.service.ModelService;
